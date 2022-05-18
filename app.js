99a4e8b390a546da1c45586c4916ee973e9626a0
wanjala stephen <stephenwanjala145@gmail.com>
2022-05-19 01:02:28 +0300

    add all depndancies

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express=require('express')
-
+const exphbs=require('express-handlebars')
+const bodyParser = require('body-parser');
+const mysql=require('mysql')
 const app=express()
+
+require('dotenv').config()
 
 const PORT=process.env.PORT||5000
 
